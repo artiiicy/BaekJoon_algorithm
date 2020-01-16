a6bc6b97a5c8cbfb5b5edd18e123cf28ac8835b4
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-01-16 14:11:19 +0900

    Create 1759_암호 만들기(BackTracking).cpp

---
+++ 1759_암호 만들기(BackTracking).cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int pw_length, num_ch, count_vowel, count_con;
+
+bool is_vowel(char input)
+{
+	if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u')
+	{
+		return 1;
+	}
+
+	else
+		return 0;
+}
+
+void make_pw(vector <char> input,  vector <char> output, int input_idx, int output_idx)
+{
+	if (output_idx == pw_length)
+		return;
+
+	for (int i = input_idx; i < num_ch; i++)
+	{
+		if (is_vowel(input[i]))
+			count_vowel++;
+		else
+			count_con++;
+
+		output[output_idx] = input[i];
+
+		if (output_idx == pw_length - 1)
+		{
+			if (count_vowel >= 1 && count_con >= 2)
+			{
+				for (int j = 0; j < pw_length; j++)
+				{
+					cout << output[j];
+				}
+				cout << endl;
+
+				if (is_vowel(input[i]))
+					count_vowel--;
+				else
+					count_con--;
+
+				continue;
+			}
+			
+			else
+			{
+				if (is_vowel(input[i]))
+					count_vowel--;
+				else
+					count_con--;
+
+				continue;
+			}
+		}
+
+		else
+		{
+			make_pw(input, output, i + 1, output_idx + 1);
+
+			if (is_vowel(input[i]))
+				count_vowel--;
+			else
+				count_con--;
+		}
+	}
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> pw_length >> num_ch;
+	vector <char> input(num_ch);
+	vector <char> output(pw_length);
+	count_vowel = 0;
+	count_con = 0;
+
+	for (int i = 0; i < num_ch; i++)
+	{
+		cin >> input[i];
+		if (is_vowel(input[i]))
+			count_vowel++;
+
+		else
+			count_con++;
+	}
+
+	// 자음, 모음 부족하면 종료.
+	if (count_vowel < 1 || count_con < 2)
+		return 0;
+
+	sort(input.begin(), input.end());
+
+	for (int i = 0; i < num_ch - pw_length + 1; i++)
+	{
+		count_vowel = 0;
+		count_con = 0;
+
+		if (is_vowel(input[i]))
+			count_vowel++;
+		else
+			count_con++;
+
+		output[0] = input[i];
+		make_pw(input, output, i+1, 1);
+	}
+}
